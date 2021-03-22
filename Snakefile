57b9c8a3760a480dadb7b565f7ceb3e8341fdf93
Ben Harris <bharris@cshl.edu>
2021-03-22 09:31:49 -0400

    adding file ext functionality to heatmaps and splitting by lineage in heatmaps

--- Snakefile
+++ Snakefile
@@ -7,17 +7,17 @@ logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
 ## Snakefile for computing metaneighbor and continuity scores
 def get_genelist_fn(wildcards):
-	fn = dataDir + 'terms/' + wildcards.term.replace(' ','_') + '_genelist.txt'
-	return fn
+    fn = dataDir + 'terms/' + wildcards.term.replace(' ','_') + '_genelist.txt'
+    return fn
 
 
 def build_save_dataset_name(dataset_name, rank_by, join_bin):
-	sdn = dataset_name
-	if rank_by is not None:
-		sdn+= '_ranked'
-	if join_bin is not None:
-		sdn += f'_joined_{join_bin}'
-	return sdn
+    sdn = dataset_name
+    if rank_by is not None:
+        sdn+= '_ranked'
+    if join_bin is not None:
+        sdn += f'_joined_{join_bin}'
+    return sdn
 
 
 
@@ -33,8 +33,8 @@ dataset_name = config['dataset_name']
 n_bins=config['n_bins']
 
 save_dataset_name = build_save_dataset_name(dataset_name, 
-	config['metaneighbor']['rank_by'], 
-	config['metaneighbor']['join_bin'])
+    config['metaneighbor']['rank_by'], 
+    config['metaneighbor']['join_bin'])
 suffix = f'{save_dataset_name}_{n_bins}'
 
 
@@ -50,23 +50,23 @@ terms = fix_terms(terms)
 logging.info(f'Running Pipeline on {terms.shape[0]} terms')
 
 rule all:
-	input:
-		expand('{dataDir}{dataset}/metaneighbor/MNUS_{term}_{suffix}.csv',
-			dataDir=dataDir,
-			dataset=dataset_name,
-			term=terms,
-			suffix=suffix),
-		expand('{dataDir}{dataset}/continuity/CONTINUITY_{term}_{suffix}.csv',
-			dataDir=dataDir,
-			dataset=dataset_name,
-			term=terms,
-			suffix=suffix),
-		expand('{figDir}{dataset}/MNUS_{term}_{suffix}.pdf', 
-			figDir = figDir,
-			dataset = dataset_name, 
-			term=terms, 
-			suffix=suffix),
-		expand(dataDir + 'terms/{term}_genelist.txt', term=terms)
+    input:
+        expand('{dataDir}{dataset}/metaneighbor/MNUS_{term}_{suffix}.csv',
+            dataDir=dataDir,
+            dataset=dataset_name,
+            term=terms,
+            suffix=suffix),
+        expand('{dataDir}{dataset}/continuity/CONTINUITY_{term}_{suffix}.csv',
+            dataDir=dataDir,
+            dataset=dataset_name,
+            term=terms,
+            suffix=suffix),
+        expand('{figDir}{dataset}/MNUS_{term}_{suffix}.pdf', 
+            figDir = figDir,
+            dataset = dataset_name, 
+            term=terms, 
+            suffix=suffix),
+        expand(dataDir + 'terms/{term}_genelist.txt', term=terms)
 
 rule prepare_terms:
     input:
@@ -79,63 +79,69 @@ rule prepare_terms:
     output:
         expand(dataDir + 'terms/{term}_genelist.txt',term=terms)
     conda:
-    	'envs/pseudotime_metaneighbor.yml'
+        'envs/pseudotime_metaneighbor.yml'
     script:
-    	'scripts/prepare_gene_lists.py'
+        'scripts/prepare_gene_lists.py'
 
 rule metaneighbor:
-	input:
-		adata_fn = config['dataset_filename'],
-		term_fn = get_genelist_fn
-	params:
-		join_bin = config['metaneighbor']['join_bin'],
-		rank_by = config['metaneighbor']['rank_by'],
-		dataset_name = dataset_name,
-		n_bins = config['n_bins'],
-		dataDir = dataDir + dataset_name + '/metaneighbor/',
-		study_col = config['metaneighbor']['study_col'],
-		pseudotime_col = config['metaneighbor']['pseudotime_col'],
-		max_genes = config['metaneighbor']['max_genes'],
-		fast_version = config['metaneighbor']['fast_version']
-	threads: config['mkl_threads']
-	output:
-		dataDir + dataset_name + '/metaneighbor/MNUS_{term}_' + suffix + '.csv'
-	conda:
-		'envs/pseudotime_metaneighbor.yml'
-	script:
-		'scripts/test_term_on_pseudotime.py'
-		
+    input:
+        adata_fn = config['dataset_filename'],
+        term_fn = get_genelist_fn
+    params:
+        join_bin = config['metaneighbor']['join_bin'],
+        rank_by = config['metaneighbor']['rank_by'],
+        dataset_name = dataset_name,
+        n_bins = config['n_bins'],
+        dataDir = dataDir + dataset_name + '/metaneighbor/',
+        study_col = config['metaneighbor']['study_col'],
+        pseudotime_col = config['metaneighbor']['pseudotime_col'],
+        max_genes = config['metaneighbor']['max_genes'],
+        fast_version = config['metaneighbor']['fast_version']
+    threads: config['mkl_threads']
+    output:
+        dataDir + dataset_name + '/metaneighbor/MNUS_{term}_' + suffix + '.csv'
+    conda:
+        'envs/pseudotime_metaneighbor.yml'
+    script:
+        'scripts/test_term_on_pseudotime.py'
+        
 
 rule continuity:
-	input:
-		mn_res = dataDir + dataset_name + '/metaneighbor/MNUS_{term}_' + suffix + '.csv'
-	params:
-		binary_threshold = config['continuity']['binary_threshold'],
-		outDir = dataDir + dataset_name + '/continuity/' ,
-		dist_func = config['continuity']['dist_func'],
-		beta = config['continuity']['beta'],
-		solf_threshold = config['continuity']['soft_threshold']
-	output:
-		dataDir + dataset_name + '/continuity/CONTINUITY_{term}_' + suffix + '.csv'
-	conda:
-		'envs/pseudotime_metaneighbor.yml'
-	threads: 1
-	script:
-		'scripts/continunity_test_on_term.py'
+    input:
+        mn_res = dataDir + dataset_name + '/metaneighbor/MNUS_{term}_' + suffix + '.csv'
+    params:
+        binary_threshold = config['continuity']['binary_threshold'],
+        outDir = dataDir + dataset_name + '/continuity/' ,
+        dist_func = config['continuity']['dist_func'],
+        beta = config['continuity']['beta'],
+        solf_threshold = config['continuity']['soft_threshold']
+    output:
+        dataDir + dataset_name + '/continuity/CONTINUITY_{term}_' + suffix + '.csv'
+    conda:
+        'envs/pseudotime_metaneighbor.yml'
+    threads: 1
+    script:
+        'scripts/continunity_test_on_term.py'
 
 rule heatmap:
-	input:
-		mn_res = dataDir + dataset_name + '/metaneighbor/MNUS_{term}_' + suffix + '.csv'
-	params:
-		out_dir = figDir + dataset_name + '/'
-	threads: 1
-	output:
-		figDir + dataset_name + '/MNUS_{term}_' + suffix + '.pdf'
-	# conda:
-	# 	'envs/pseudotime_metaneighbor_r.yml'
-	# log:
-	# 	'log/heatmap_{term}.log'
-	# script:
-	# 	'scripts/metaneighbor_pseudotime_heatmap.R'
-	shell:
-		config['Rscript'] + ' scripts/metaneighbor_pseudotime_heatmap.R --fn {input.mn_res} --outdir {params.out_dir}'
+    input:
+        mn_res = dataDir + dataset_name + '/metaneighbor/MNUS_{term}_' + suffix + '.csv'
+    params:
+        out_dir = figDir + dataset_name + '/',
+        split_lineages = config['heatmap']['split_lineages'],
+        extension = config['heatmap']['extension']
+    threads: 1
+    output:
+        figDir + dataset_name + '/MNUS_{term}_' + suffix + '.{params.extension}' 
+    # conda:
+    #   'envs/pseudotime_metaneighbor_r.yml'
+    # log:
+    #   'log/heatmap_{term}.log'
+    # script:
+    #   'scripts/metaneighbor_pseudotime_heatmap.R'
+    shell:
+        config['Rscript'] + """scripts/metaneighbor_pseudotime_heatmap.R \
+                                --fn {input.mn_res} \
+                                --outdir {params.out_dir} \
+                                --split-lineages {params.split_lineages} \
+                                --file-ext {params.extension} """
--- scripts/metaneighbor_pseudotime_heatmap.R
+++ scripts/metaneighbor_pseudotime_heatmap.R
@@ -1,115 +1,185 @@
 
-`%>%`<-magrittr::`%>%`
-#log<-file('log/test_log.log', open='wt')
+
+
+
+
+
+`%>%` <- magrittr::`%>%`
+# log<-file('log/test_log.log', open='wt')
 # log <- file(snakemake@log[[1]], open="wt")
 # sink(log, type="message")
 
+parser <-
+  argparser::arg_parser(description = "Arguments for Heatmap generation")
+parser <-
+  argparser::add_argument(parser, "--fn", help = "Filename with MetaNeighbor Output")
+parser <-
+  argparser::add_argument(parser, "--outdir", help = "Directory to store figure files in")
+parser <-
+  argparser::add_argument(parser,
+                          "--split-lineages",
+                          default = 0,
+                          help = "Argument to split heatmaps into each lineage, pass non-zero for T")
+parser <-
+  argparser::add.argument(parser, '--file-ext', default = 'png', help = 'Extensio to use for saving, png or pdf')
 
-parser <- argparser::arg_parser(description = "Arguments for Heatmap generation")
-parser <- argparser::add_argument(parser, "--fn", help = "Filename with MetaNeighbor Output")
-parser <- argparser::add_argument(parser, "--outdir", help ='Directory to store figure files in')
 args <- argparser::parse_args(parser)
 fn <- args$fn
-outdir <-args$outdir
+outdir <- args$outdir
+split_lineages <- as.logical(args$split_lineages)
+extension <- args$file_ext
 
-build_annotation <- function(meta, side = 'column') {
-    cols <- colnames(meta)
-    color_list <- vector(mode = "list", length = length(cols))
-    if ('Lineage' %in% cols) {
-        lins<-unique(meta$Lineage)
-        pal<-RColorBrewer::brewer.pal(length(lins),'Dark2')
-        if (length(pal) > length(lins)){ 
-          #RColorBrewer won't return less than 3 colors
-          pal<-pal[1:length(lins)]
-        }
-        names(pal)<-lins
-        color_list$Lineage <- pal
+if (!extension %in% c('pdf', 'png')) {
+  message('File Extension Passed was neither pdf or png, setting extension to png')
+  extension <- 'png'
+}
+build_annotation <- function(meta, side = "column") {
+  cols <- colnames(meta)
+  color_list <- vector(mode = "list", length = length(cols))
+  if ("Lineage" %in% cols) {
+    lins <- unique(meta$Lineage)
+    pal <- RColorBrewer::brewer.pal(length(lins), "Dark2")
+    if (length(pal) > length(lins)) {
+      # RColorBrewer won't return less than 3 colors
+      pal <- pal[1:length(lins)]
     }
-    if ('Study' %in% cols) {
-        studies <- unique(meta$Study)
-        pal <- RColorBrewer::brewer.pal(length(studies), 'Set3')
-        names(pal) <- studies
-        color_list$Study <- setNames(pal, studies)
-    }
-    if ('Pseudotime.Bin' %in% cols) {
-        n = nrow(meta)
-        min_v = min(meta[, 'Pseudotime.Bin'])
-        max_v = max(meta[, 'Pseudotime.Bin'])
-        Var = circlize::colorRamp2(seq(min_v, max_v, length = n),
-                                   rev(hcl.colors(n, "Blues")))
-        
-        color_list$Pseudotime.Bin <- Var
-        
-    }
-    annot <-
-        ComplexHeatmap::HeatmapAnnotation(
-            df = meta,
-            col = color_list,
-            which = side,
-            show_legend = side == 'column',
-            show_annotation_name = F
-        )
-    annot
+    names(pal) <- lins
+    color_list$Lineage <- pal
+  }
+  if ("Study" %in% cols) {
+    studies <- unique(meta$Study)
+    pal <- RColorBrewer::brewer.pal(length(studies), "Set3")
+    names(pal) <- studies
+    color_list$Study <- setNames(pal, studies)
+  }
+  if ("Pseudotime.Bin" %in% cols) {
+    n <- nrow(meta)
+    min_v <- min(meta[, "Pseudotime.Bin"])
+    max_v <- max(meta[, "Pseudotime.Bin"])
+    Var <- circlize::colorRamp2(seq(min_v, max_v, length = n),
+                                rev(hcl.colors(n, "Blues")))
+    
+    color_list$Pseudotime.Bin <- Var
+  }
+  annot <-
+    ComplexHeatmap::HeatmapAnnotation(
+      df = meta,
+      col = color_list,
+      which = side,
+      show_legend = side == "column",
+      show_annotation_name = F
+    )
+  annot
 }
 
 compute_splits <- function(nbins, nstudies) {
-    rep(1:nstudies, each = nbins)
+  rep(1:nstudies, each = nbins)
 }
 
 build_heatmap <- function(mat,
+                          figfile,
                           col_meta = NULL,
                           row_meta = NULL,
                           row_title = NULL,
                           column_title = NULL,
                           split = NULL) {
-    if (!is.null(col_meta)) {
-        col_meta <- build_annotation(col_meta)
-    }
-    if (!is.null(row_meta)) {
-        row_meta <- build_annotation(row_meta, side = 'row')
-    }
+  message(figfile)
+  
+  if (!is.null(col_meta)) {
+    col_meta <- build_annotation(col_meta)
+  }
+  if (!is.null(row_meta)) {
+    row_meta <- build_annotation(row_meta, side = "row")
+  }
+  
+  if (tools::file_ext(figfile) == 'png'){
+    png(file = figfile,
+      width = 700,
+      height = 500)
+  } elif (tools::figfile(figfile)=='pdf'){
+    pdf(file=figfile,
+        width = 7,
+        height = 5)
+  }
+  
+  print({
     mat %>% ComplexHeatmap::Heatmap(
-        .,
-        cluster_rows = F,
-        cluster_columns = F,
-        show_column_names = F,
-        show_row_names = F,
-        row_title = row_title,
-        column_title = column_title,
-        row_order = 1:dim(.)[1],
-        column_order = 1:dim(.)[1],
-        top_annotation = col_meta,
-        width = 1,
-        height = 1,
-        left_annotation = row_meta,
-        col = circlize::colorRamp2(c(0, .5, 1), c('blue', 'white', 'red')),
-        row_split = split,
-        column_split = split
+      .,
+      cluster_rows = F,
+      cluster_columns = F,
+      show_column_names = F,
+      show_row_names = F,
+      row_title = row_title,
+      column_title = column_title,
+      row_order = 1:dim(.)[1],
+      column_order = 1:dim(.)[1],
+      top_annotation = col_meta,
+      width = 1,
+      height = 1,
+      left_annotation = row_meta,
+      col = circlize::colorRamp2(c(0, .5, 1), c("blue", "white", "red")),
+      row_split = split,
+      column_split = split
     )
+  })
+  dev.off()
 }
 
 
 df <- read.csv(fn)
 results_name <- tools::file_path_sans_ext(fn)
-outfile <- paste(outdir, basename(results_name), ".pdf", sep = "")
+outfile <-
+  paste(outdir, basename(results_name), '.', extension, sep = "")
 mat <- df %>%
   dplyr::select(!c(Pseudotime.Bin, Study, Lineage)) %>%
   as.matrix()
-mat <- mat[, -1]
+mat <- mat[,-1]
 
 meta <- df %>% dplyr::select(c(Lineage, Study, Pseudotime.Bin))
 n_lins <- meta$Lineage %>%
   unique() %>%
   length()
+
 title <- basename(results_name)
-message(outfile)
-pdf(file = outfile, width = 7, height = 5)
+
+n_bins <-
+  max(meta[, "Pseudotime.Bin"]) - min(meta[, "Pseudotime.Bin"]) + 1
+n_studies <- length(unique(meta$Study))
+
+
 if (n_lins > 1) {
-  build_heatmap(mat, col_meta = meta, row_meta = meta[, "Lineage", drop = F], column_title = title)
+  build_heatmap(
+    mat,
+    outfile,
+    col_meta = meta,
+    row_meta = meta[, "Lineage", drop = F],
+    column_title = title
+  )
+  if (split_lineages) {
+    lins <- unique(meta$Lineage)
+    for (lin in lins) {
+      slice <- meta$Lineage == lin
+      
+      outf <- tools::file_path_sans_ext(outfile)
+      outf <- paste0(outf, '_', lin, '.', extension)
+      
+      build_heatmap(
+        mat[slice, slice],
+        outf,
+        col_meta = meta[slice, c('Study', 'Pseudotime.Bin')],
+        split = compute_splits(n_bins, n_studies),
+        column_title = paste(lin, title)
+      )
+    }
+  }
+  
+  
 } else {
-  build_heatmap(mat,
-    col_meta = meta[, c("Study", "Pseudotime.Bin")], column_title = title,
-    split = compute_splits(16, 9)
+  build_heatmap(
+    mat,
+    outfile,
+    col_meta = meta[, c("Study", "Pseudotime.Bin")],
+    column_title = title,
+    split = compute_splits(n_bins, n_studies)
   )
 }
-dev.off()
